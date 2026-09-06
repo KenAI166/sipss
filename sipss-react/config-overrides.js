26fbc378b3f3047c706478bd224ae4527c85038c
kennn <kengabiola12@gmail.com>
2026-09-06 14:31:25 +0800

    fixed

--- sipss-react/config-overrides.js
+++ sipss-react/config-overrides.js
@@ -5,7 +5,6 @@ module.exports = function override(config) {
   config.resolve.alias = {
     ...config.resolve.alias,
     "@": path.resolve(__dirname, "src"),
-    "xlsx": path.resolve(__dirname, "node_modules/xlsx/dist/xlsx.full.min.js"),
   };
   return config;
 };
